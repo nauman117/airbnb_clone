6e24c04496ccb71ef0f5df6aa7b8d484aea6d685
Nauman Ahmed <nauman.ahmed@CMDLHRLT843>
2023-08-15 04:53:23 -0500

    Navbar UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/Components/Navbar/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
